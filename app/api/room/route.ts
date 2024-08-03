132e0434a2dbfac10c707e4503854e1287cac97e
Mr. David <programmerdatch@gmail.com>
2024-08-03 23:13:37 +0300

    remove-cache

--- app/api/room/route.ts
+++ app/api/room/route.ts
@@ -5,8 +5,10 @@ export async function GET() {
     try {
         const messages = await prisma.message.findMany();
         const rooms = Array.from(new Set(messages.map(m => m.room)))
-        return NextResponse.json({ message: "All rooms", data: rooms }, { status: 200 })
-    }
+        const response = NextResponse.json({ message: "All rooms", data: rooms }, { status: 200 });
+        response.headers.set('Cache-Control', 'no-store');
+
+        return response;    }
     catch (error: any) {
         return NextResponse.json({ message: error.message || "Failed to get rooms" }, { status: 500 })
     }
